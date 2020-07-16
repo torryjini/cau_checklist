ca97fb6a4a2e33adb84038fd98ef5f595d1dd20d
torryjini <torryjini@users.noreply.github.com>
2020-07-16 11:15:21 +0900

    Add files via upload

--- checklist.js
+++ checklist.js
@@ -1,3 +1,26 @@
+//Get the button
+var mybutton = document.getElementById("TopBtn");
+
+// When the user scrolls down 10px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
 function maxLengthCheck(object) {
   if (object.value.length > object.maxLength) {
     object.value = object.value.slice(0, object.maxLength);
