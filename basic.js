ff09f16e91b6cad5d38a2e0b78dea4eaf5124f4d
torryjini <torryjini@users.noreply.github.com>
2020-08-03 11:04:09 +0900

    Add files via upload

--- basic.js
+++ basic.js
@@ -27,24 +27,6 @@ function maxLengthCheck(object) {
   }
 }
 
-function onlyNumber(event) {
-  event = event || window.event;
-  var keyID = (event.which) ? event.which : event.keyCode;
-  if ((keyID >= 48 && keyID <= 57) || (keyID >= 96 && keyID <= 105) || keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39)
-    return;
-  else
-    return false;
-}
-
-function removeChar(event) {
-  event = event || window.event;
-  var keyID = (event.which) ? event.which : event.keyCode;
-  if (keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39)
-    return;
-  else
-    event.target.value = event.target.value.replace(/[^0-9]/g, "");
-}
-
 function Form_Reset(id) {
   document.getElementById(id).reset();
 }
