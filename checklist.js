ac3ebe19fc6e753c814971217fe275f74a5fc58e
torryjini <torryjini@users.noreply.github.com>
2020-08-04 10:46:19 +0900

    Add files via upload

--- checklist.js
+++ checklist.js
@@ -259,11 +259,7 @@ function BaseNeceresult(Majorstandard) {
     return false;
   }
 
-  if (AdYear == 2016 && Majortext.includes("행정")) {
-    if (1 >= BaseStandard - Basepoint) {
-      word += "전공기초 : 기준 통과\n";
-    }
-  } else if (Basepoint < BaseStandard) {
+  if (Basepoint < BaseStandard) {
     word += "전공기초 : " + [BaseStandard - Basepoint] + " 학점 미달\n";
   } else {
     word += "전공기초 : 기준 통과\n";
