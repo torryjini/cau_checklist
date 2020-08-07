395fe684fad581eba8e3d3833401d64f00d1249e
torryjini <torryjini@users.noreply.github.com>
2020-08-07 17:31:54 +0900

    Add files via upload

---
+++ chi/basic-ch.js
@@ -0,0 +1,384 @@
+//Get the button
+var mybutton = document.getElementById("TopBtn");
+
+// When the user scrolls down 10px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+function maxLengthCheck(object) {
+  if (object.value.length > object.maxLength) {
+    object.value = object.value.slice(0, object.maxLength);
+  }
+}
+
+function Form_Reset(id) {
+  document.getElementById(id).reset();
+}
+
+function total_point_sum() {
+  var LiberalTotal = Number(document.getElementById("liberal_total").value);
+  var Basepoint = Number(document.getElementById("base_input").value);
+  var Majorpoint = Number(document.getElementById("major_input").value);
+  var Doublepoint = Number(document.getElementById("double_major_point").value);
+  var Linkpoint = Number(document.getElementById("link_major_point").value);
+  var Fusionpoint = Number(document.getElementById("fusion_major_point").value);
+  var Planpoint = Number(document.getElementById("plan_major_point").value);
+  var Minorpoint = Number(document.getElementById("minor_point").value);
+  var Freepoint = Number(document.getElementById("free_point").value);
+  var Teachpoint = Number(document.getElementById("teaching_point").value);
+  if (LiberalTotal > 45) {
+    document.getElementById("the_total").value = 45 + Basepoint + Majorpoint + Doublepoint + Linkpoint + Fusionpoint + Planpoint + Minorpoint + Freepoint + Teachpoint;
+  } else {
+    document.getElementById("the_total").value = LiberalTotal + Basepoint + Majorpoint + Doublepoint + Linkpoint + Fusionpoint + Planpoint + Minorpoint + Freepoint + Teachpoint;
+  }
+}
+
+function Major_result() {
+  var AdYear = Number(document.getElementById("ad-year").value);
+  var Majorstandard = document.getElementById("MultiMajor_select").value;
+  var MajorSelect = document.getElementById("Major_select").value;
+  var LinkCross = Number(document.getElementById("cross_point1").value);
+  var Fusion = document.getElementById("fusion-options");
+  var Fusiontext = Fusion.options[Fusion.selectedIndex].text;
+  var FusionCross = Number(document.getElementById("cross_point2").value);
+  var PlanCross = Number(document.getElementById("cross_point3").value);
+
+  if (AdYear < 1000) {
+    document.getElementById("student-info").scrollIntoView();
+    swal("입학연도를 선택하세요!", "", "error");
+    return false;
+  }
+
+  if (Fusiontext.includes("문화") || Fusiontext.includes("창업") || Fusiontext.includes("게임")) {
+    if (FusionCross > 6) {
+      document.getElementById("double_major_none").scrollIntoView();
+      swal(Fusiontext + " 융합전공 교차인정\n최대학점은 6 학점입니다!", "", "error")
+      return false;
+    }
+  } else if (Fusiontext.includes("금융")) {
+    if (FusionCross > 15) {
+      document.getElementById("double_major_none").scrollIntoView();
+      swal(Fusiontext + " 융합전공 교차인정\n최대학점은 15 학점입니다!", "", "error")
+      return false;
+    }
+  } else if (Fusiontext.includes("인문") || Fusiontext.includes("보안") || Fusiontext.includes("테크") || Fusiontext.includes("벤처")) {
+    if (FusionCross > 12) {
+      document.getElementById("double_major_none").scrollIntoView();
+      swal(Fusiontext + " 융합전공 교차인정\n최대학점은 12 학점입니다!", "", "error")
+      return false;
+    }
+  }
+
+  if (MajorSelect == "none") {
+    document.getElementById("student-info").scrollIntoView();
+    swal("전공 학과를 선택하세요!", "", "error");
+    return false;
+  } else if (Majorstandard < 45) {
+    document.getElementById("student-info").scrollIntoView();
+    swal("다전공 여부를 선택하세요!", "", "error");
+    return false;
+  } else if (LinkCross > 12) {
+    document.getElementById("double_major_point").scrollIntoView();
+    swal("연계전공의 교차인정 최대학점은 12 학점입니다!", "", "error")
+    return false;
+  } else if (PlanCross > 6) {
+    document.getElementById("link_major_none").scrollIntoView();
+    swal("자기설계전공의 교차인정 최대학점은 6 학점입니다!", "", "error")
+    return false;
+  } else {
+    BaseNeceresult(Majorstandard);
+  }
+}
+
+function OfficeNumber() {
+  var Office = {
+    "psyche": "심리학과",
+    "politics": "정치국제학과",
+    "lis": "문헌정보학과",
+    "socialwelfare": "사회복지학부",
+    "cmc": "미디어커뮤니케이션학부",
+    "cmc2": "미디어커뮤니케이션학부",
+    "planning": "도시계획/부동산학과",
+    "sociology": "사회학과",
+    "public-admin": "공공인재학부",
+    "public-policy": "공공인재학부",
+    "public": "공공인재학부"
+  };
+  var PhoneNumber = {
+    "psyche": "02-820-5124",
+    "politics": "02-820-5473",
+    "lis": "02-820-5144",
+    "socialwelfare": "02-820-5149",
+    "cmc": "02-820-5481",
+    "cmc2": "02-820-5481",
+    "planning": "02-820-5108",
+    "sociology": "02-820-6351",
+    "public-admin": "02-820-5445",
+    "public-policy": "02-820-5445",
+    "public": "02-820-5445"
+  };
+  var Major = document.getElementById("Major_select").value;
+  if (Major == "none") {
+    document.getElementById("dept").value = "-";
+    document.getElementById("phone").value = "-";
+  } else {
+    document.getElementById("dept").value = Office[Major];
+    document.getElementById("phone").value = PhoneNumber[Major];
+  }
+}
+
+function checkcont1(none, point) {
+  var none_checked = document.getElementById(none).checked;
+  if (!none_checked) {
+    document.getElementById(point).readOnly = false;
+  } else {
+    document.getElementById(point).value = null;
+    document.getElementById(point).readOnly = true;
+  }
+}
+
+function checkcont(none, point, cross) {
+  var none_checked = document.getElementById(none).checked;
+  if (!none_checked) {
+    document.getElementById(point).readOnly = false;
+    document.getElementById(cross).readOnly = false;
+  } else {
+    document.getElementById(point).value = null;
+    document.getElementById(point).readOnly = true;
+    document.getElementById(cross).value = null;
+    document.getElementById(cross).readOnly = true;
+  }
+}
+
+function FusionStandardCheck() {
+  var fusion_min = document.getElementById("fusion-options").value;
+  var crosspoint = document.getElementById("cross_point2");
+  if (fusion_min == 45) {
+    document.getElementById("fusion_standard").value = 45;
+    crosspoint.setAttribute("max", 15);
+  } else {
+    document.getElementById("fusion_standard").value = 36;
+    crosspoint.setAttribute("max", 12);
+  }
+}
+
+function major_standard_maker() {
+  var Multimajor_standard = Number(document.getElementById("MultiMajor_select").value);
+  document.getElementById("major_standard").value = Multimajor_standard;
+  var Multimajor = document.getElementById("MultiMajor_select");
+  var Multimajortext = Multimajor.options[Multimajor.selectedIndex].text;
+
+  if (Multimajortext.includes("심화")) {
+    document.getElementById("double_major_none").checked = true;
+    document.getElementById("double_major_point").value = null;
+    document.getElementById("double_major_point").readOnly = true;
+    document.getElementById("double_major_paper").checked = true;
+
+    document.getElementById("link_major_none").checked = true;
+    document.getElementById("link_major_point").value = null;
+    document.getElementById("link_major_point").readOnly = true;
+    document.getElementById("cross_point1").value = null;
+    document.getElementById("cross_point1").readOnly = true;
+
+    document.getElementById("fusion_major_none").checked = true;
+    document.getElementById("fusion_major_point").value = null;
+    document.getElementById("fusion_major_point").readOnly = true;
+    document.getElementById("cross_point2").value = null;
+    document.getElementById("cross_point2").readOnly = true;
+
+    document.getElementById("plan_major_none").checked = true;
+    document.getElementById("plan_major_point").value = null;
+    document.getElementById("plan_major_point").readOnly = true;
+    document.getElementById("cross_point3").value = null;
+    document.getElementById("cross_point3").readOnly = true;
+  } else if (Multimajortext.includes("복수")) {
+    document.getElementById("double_major_none").checked = false;
+    document.getElementById("double_major_point").readOnly = false;
+    document.getElementById("double_major_paper").checked = false;
+
+    document.getElementById("link_major_none").checked = true;
+    document.getElementById("link_major_point").value = null;
+    document.getElementById("link_major_point").readOnly = true;
+    document.getElementById("cross_point1").value = null;
+    document.getElementById("cross_point1").readOnly = true;
+
+    document.getElementById("fusion_major_none").checked = true;
+    document.getElementById("fusion_major_point").value = null;
+    document.getElementById("fusion_major_point").readOnly = true;
+    document.getElementById("cross_point2").value = null;
+    document.getElementById("cross_point2").readOnly = true;
+
+    document.getElementById("plan_major_none").checked = true;
+    document.getElementById("plan_major_point").value = null;
+    document.getElementById("plan_major_point").readOnly = true;
+    document.getElementById("cross_point3").value = null;
+    document.getElementById("cross_point3").readOnly = true;
+  } else if (Multimajortext.includes("연계")) {
+    document.getElementById("double_major_none").checked = true;
+    document.getElementById("double_major_point").value = null;
+    document.getElementById("double_major_point").readOnly = true;
+    document.getElementById("double_major_paper").checked = true;
+
+    document.getElementById("link_major_none").checked = false;
+    document.getElementById("link_major_point").readOnly = false;
+    document.getElementById("cross_point1").readOnly = false;
+
+    document.getElementById("fusion_major_none").checked = true;
+    document.getElementById("fusion_major_point").value = null;
+    document.getElementById("fusion_major_point").readOnly = true;
+    document.getElementById("cross_point2").value = null;
+    document.getElementById("cross_point2").readOnly = true;
+
+    document.getElementById("plan_major_none").checked = true;
+    document.getElementById("plan_major_point").value = null;
+    document.getElementById("plan_major_point").readOnly = true;
+    document.getElementById("cross_point3").value = null;
+    document.getElementById("cross_point3").readOnly = true;
+  } else if (Multimajortext.includes("융합")) {
+    document.getElementById("double_major_none").checked = true;
+    document.getElementById("double_major_point").value = null;
+    document.getElementById("double_major_point").readOnly = true;
+    document.getElementById("double_major_paper").checked = true;
+
+    document.getElementById("link_major_none").checked = true;
+    document.getElementById("link_major_point").value = null;
+    document.getElementById("link_major_point").readOnly = true;
+    document.getElementById("cross_point1").value = null;
+    document.getElementById("cross_point1").readOnly = true;
+
+    document.getElementById("fusion_major_none").checked = false;
+    document.getElementById("fusion_major_point").readOnly = false;
+    document.getElementById("cross_point2").readOnly = false;
+
+    document.getElementById("plan_major_none").checked = true;
+    document.getElementById("plan_major_point").value = null;
+    document.getElementById("plan_major_point").readOnly = true;
+    document.getElementById("cross_point3").value = null;
+    document.getElementById("cross_point3").readOnly = true;
+  } else if (Multimajortext.includes("설계")) {
+    document.getElementById("double_major_none").checked = true;
+    document.getElementById("double_major_point").value = null;
+    document.getElementById("double_major_point").readOnly = true;
+    document.getElementById("double_major_paper").checked = true;
+
+    document.getElementById("link_major_none").checked = true;
+    document.getElementById("link_major_point").value = null;
+    document.getElementById("link_major_point").readOnly = true;
+    document.getElementById("cross_point1").value = null;
+    document.getElementById("cross_point1").readOnly = true;
+
+    document.getElementById("fusion_major_none").checked = true;
+    document.getElementById("fusion_major_point").value = null;
+    document.getElementById("fusion_major_point").readOnly = true;
+    document.getElementById("cross_point2").value = null;
+    document.getElementById("cross_point2").readOnly = true;
+
+    document.getElementById("plan_major_none").checked = false;
+    document.getElementById("plan_major_point").readOnly = false;
+    document.getElementById("cross_point3").readOnly = false;
+  } else if (Multimajortext.includes("선 택")) {
+    document.getElementById("double_major_none").checked = false;
+    document.getElementById("double_major_point").readOnly = false;
+    document.getElementById("double_major_paper").checked = false;
+
+    document.getElementById("link_major_none").checked = false;
+    document.getElementById("link_major_point").readOnly = false;
+    document.getElementById("cross_point1").readOnly = false;
+
+    document.getElementById("fusion_major_none").checked = false;
+    document.getElementById("fusion_major_point").readOnly = false;
+    document.getElementById("cross_point2").readOnly = false;
+
+    document.getElementById("plan_major_none").checked = false;
+    document.getElementById("plan_major_point").readOnly = false;
+    document.getElementById("cross_point3").readOnly = false;
+  }
+}
+
+// 여기서부터는 졸업인정제 등 졸업요건 관련
+
+function etcmodal() {
+  var word = "기타 졸업요건"
+  var content = "졸업가능 : 학점 이수 기준 모두 통과 + 졸업인정제 통과 + 졸업시험/논문 합격 + 최저 평점 기준 통과\n\n"
+   + "수료 : 학점 이수 기준은 모두 통과했으나 졸업인정제(영어, 한자, 한국어)와 졸업시험/논문 중 하나라도 미통과시 수료\n\n"
+   + "졸업불가 : 학점 이수 기준 중 하나라도 부족하거나 전 학년 평점이 2.0 미만인 경우 졸업불가"
+  swal(word, content, "info");
+}
+
+function Etc_result() {
+  var English_result = document.getElementById("English").checked;
+  var Hanja_result = document.getElementById("Hanja").checked;
+  var Korean_result = document.getElementById("Korean").checked;
+  var Paper_result = document.getElementById("major_paper").checked;
+  var Paper_result2 = document.getElementById("double_major_paper").checked;
+  var Average_result = Number(document.getElementById("average").value);
+
+  if (Average_result > 4.5) {
+    swal("평균평점은 4.5 만점입니다!", "", "error")
+    return false;
+  }
+
+  var word = "졸업인정제 통과 여부\n";
+
+  if (!English_result) {
+    word += "- 영어 : 미통과\n"
+  } else {
+    word += "- 영어 : 통과\n"
+  }
+
+  if (Hanja_result) {
+    word += "- 한자 : 통과\n"
+  } else {
+    word += "- 한자 : 미통과\n"
+  }
+
+  if (Korean_result) {
+    word += "- 한국어 : 통과\n"
+  } else {
+    word += "- 한국어 : 미통과\n"
+  }
+
+  if (Paper_result) {
+    word += "졸업시험/논문\n- 주전공 : 합격\n"
+  } else {
+    word += "졸업시험/논문\n- 주전공 : 불합격\n"
+  }
+
+  if (Paper_result2) {
+    word += "- 복수전공 : 통과/해당없음\n"
+  } else {
+    word += "- 복수전공 : 불합격\n"
+  }
+
+  if (Average_result >= 2) {
+    word += "평균평점 : 통과"
+  } else {
+    word += "평균평점 : 미통과_졸업불가"
+  }
+
+  if (English_result && Hanja_result && Korean_result && Paper_result && Paper_result2 && Average_result >= 2) {
+    swal("기타 졸업요건 결과", word, "success");
+  } else if (Average_result >= 2) {
+    if (!English_result || !Hanja_result || !Korean_result || !Paper_result || !Paper_result2) {
+      swal("기타 졸업요건 결과", word, "warning");
+    }
+  } else if(Average_result <= 2) {
+    swal("기타 졸업요건 결과", word, "error");
+  }
+}
