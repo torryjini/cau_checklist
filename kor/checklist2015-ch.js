5859b0e743f775a09c06249b07293f01902f5a20
torryjini <torryjini@users.noreply.github.com>
2020-12-11 18:26:42 +0900

    언어별 리팩토링

---
+++ kor/checklist2015-ch.js
@@ -0,0 +1,249 @@
+// 여기서부터는 전공영역
+
+function BaseNeceresult(Majorstandard) {
+  var AdYear = Number(document.getElementById("ad-year").value);
+  var Major = document.getElementById("Major_select");
+  var Majortext = Major.options[Major.selectedIndex].text;
+  var Multimajor = document.getElementById("MultiMajor_select");
+  var Multimajortext = Multimajor.options[Multimajor.selectedIndex].text;
+  var BaseStandard = document.getElementById("base_standard").value;
+  var NeceStandard = document.getElementById("nece_standard").value;
+  var Basepoint = Number(document.getElementById("base_input").value);
+  var Necepoint = Number(document.getElementById("nece_input").value);
+  var Majorpoint = Number(document.getElementById("major_input").value);
+  Majorstandard = Number(Majorstandard);
+  var word = "👉入学年度 : " + AdYear + "年";
+
+  if (Majortext.includes("트랙")) {
+    word += "\n👉主专业 : 공공인재학부" + Majortext + "\n👉多专业 : " + Multimajortext + "\n";
+  } else {
+    word += "\n👉主专业 : " + Majortext + "\n👉多专业 : " + Multimajortext + "\n";
+  }
+
+  var FusionOption = Number(document.getElementById("fusion-options").value);
+  if (Multimajortext.includes("融合")) {
+    if (FusionOption == 0) {
+      swal("请选择细部融合专业（세부융합전공）!", "", "error");
+      document.getElementById("double_major_none").scrollIntoView();
+      return false;
+    }
+  }
+
+  if (Majorpoint < Necepoint) {
+    swal("专业课程包括专业必修!", "", "error");
+    return false;
+  }
+
+  if (Basepoint < BaseStandard) {
+    word += "👉专业基础 : 缺少" + [BaseStandard - Basepoint] + " 学分\n";
+  } else {
+    word += "👉专业基础 : 通过\n";
+  }
+
+  if (Necepoint < NeceStandard) {
+    word += "👉专业必修 : 缺少" + [NeceStandard - Necepoint] + " 学分\n";
+  } else {
+    word += "👉专业必修 : 通过\n";
+  }
+
+  if (Majorpoint < Majorstandard) {
+    word += "👉专业课程 : 缺少" + [Majorstandard - Majorpoint] + " 学分\n"
+  } else {
+    word += "👉专业课程 : 通过\n"
+  }
+
+  var Doublecheck = document.getElementById("double_major_none").checked;
+  var Doublepoint = Number(document.getElementById("double_major_point").value);
+  if (!Doublecheck) {
+    if (Doublepoint < 45) {
+      word += "👉双专业 : 缺少" + [45 - Doublepoint] + " 学分\n"
+    } else {
+      word += "👉双专业 : 通过最少学分\n"
+    }
+  }
+
+  var Linkcheck = document.getElementById("link_major_none").checked;
+  var LinkCross = Number(document.getElementById("cross_point1").value);
+  var Linkpoint = Number(document.getElementById("link_major_point").value) + LinkCross;
+  if (!Linkcheck) {
+    if (Linkpoint < 36) {
+      word += "👉연계전공 : 缺少" + [36 - Linkpoint] + " 学分\n"
+    } else {
+      word += "👉연계전공 : 通过最少学分\n"
+    }
+  }
+
+  var Fusioncheck = document.getElementById("fusion_major_none").checked;
+  var Fusion = document.getElementById("fusion-options");
+  var Fusiontext = Fusion.options[Fusion.selectedIndex].text;
+  var Fusionpoint = Number(document.getElementById("fusion_major_point").value) + Number(document.getElementById("cross_point2").value);
+  var FusionStandard = Number(document.getElementById("fusion_standard").value);
+  if (!Fusioncheck) {
+    if (Fusionpoint < FusionStandard) {
+      word += "👉" + Fusiontext + "융합전공 : 缺少" + [FusionStandard - Fusionpoint] + " 学分\n"
+    } else {
+      word += "👉" + Fusiontext + "융합전공 : 通过最少学分\n"
+    }
+  }
+
+  var Plancheck = document.getElementById("plan_major_none").checked;
+  var Planpoint = Number(document.getElementById("plan_major_point").value) + Number(document.getElementById("cross_point3").value);
+  if (!Plancheck) {
+    if (Planpoint < 36) {
+      word += "👉설계전공 : 缺少" + [36 - Planpoint] + " 学分\n"
+    } else {
+      word += "👉설계전공 : 通过最少学分\n"
+    }
+  }
+
+  var Minorcheck = document.getElementById("minor_none").checked;
+  var Minorpoint = Number(document.getElementById("minor_point").value);
+  if (!Minorcheck) {
+    if (Minorpoint < 21) {
+      word += "👉부전공 : 缺少" + [21 - Minorpoint] + " 学分\n"
+    } else {
+      word += "👉부전공 : 通过最少学分\n"
+    }
+  }
+
+  var Freepoint = Number(document.getElementById("free_point").value);
+  word += "👉자유선택 : " + Freepoint + " 学分\n"
+
+  var Teachingcheck = document.getElementById("teaching_none").checked;
+  var Teachingpoint = Number(document.getElementById("teaching_point").value);
+  if (!Teachingcheck) {
+    if (Teachingpoint < 22)
+      word += "👉교직이수 :  缺少" + [22 - Teachingpoint] + " 学分\n"
+    else {
+      word += "👉교직이수 : 通过最少学分\n"
+    }
+  }
+
+  var TotalStandard = Number(document.getElementById("total_standard").value);
+  var TotalPoint = Number(document.getElementById("the_total").value);
+  if (TotalPoint < TotalStandard) {
+    word += "👉总进修学分 : 缺少" + [TotalStandard - TotalPoint] + " 学分"
+  } else {
+    word += "👉总进修学分 : 通过"
+  }
+
+  if (Basepoint >= BaseStandard && Necepoint >= NeceStandard && Majorpoint >= Majorstandard && TotalPoint >= TotalStandard) {
+    if (Minorcheck && Teachingcheck) {
+      if (Multimajortext.includes("심화")) {
+        swal("专业领域结果", word, "success")
+      } else if (Multimajortext.includes("복수")) {
+        if (Doublepoint >= 45) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("연계")) {
+        if (Linkpoint >= 36) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("융합")) {
+        if (Fusionpoint >= FusionStandard) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("설계")) {
+        if (Planpoint >= 36) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      }
+    } else if (Minorpoint >= 21 && Teachingcheck) {
+      if (Multimajortext.includes("심화")) {
+        swal("专业领域结果", word, "success")
+      } else if (Multimajortext.includes("복수")) {
+        if (Doublepoint >= 45) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("연계")) {
+        if (Linkpoint >= 36) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("융합")) {
+        if (Fusionpoint >= FusionStandard) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("설계")) {
+        if (Planpoint >= 36) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      }
+    } else if (Minorpoint >= 21 && Teachingpoint >= 22) {
+      if (Multimajortext.includes("심화")) {
+        swal("专业领域结果", word, "success")
+      } else if (Multimajortext.includes("복수")) {
+        if (Doublepoint >= 45) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("연계")) {
+        if (Linkpoint >= 36) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("융합")) {
+        if (Fusionpoint >= FusionStandard) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("설계")) {
+        if (Planpoint >= 36) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      }
+    } else if (Minorcheck && Teachingpoint >= 22) {
+      if (Multimajortext.includes("심화")) {
+        swal("专业领域结果", word, "success")
+      } else if (Multimajortext.includes("복수")) {
+        if (Doublepoint >= 45) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("연계")) {
+        if (Linkpoint >= 36) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("융합")) {
+        if (Fusionpoint >= FusionStandard) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      } else if (Multimajortext.includes("설계")) {
+        if (Planpoint >= 36) {
+          swal("专业领域结果", word, "success")
+        } else {
+          swal("专业领域结果", word, "error")
+        }
+      }
+    } else {
+      swal("专业领域结果", word, "error")
+    }
+  } else {
+    swal("专业领域结果", word, "error")
+  }
+}
